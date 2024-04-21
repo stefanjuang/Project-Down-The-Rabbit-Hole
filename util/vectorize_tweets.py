92db3d797a719ec0e7ef8f7bd3418a4f299e69c9
tthuwng <tronghung199@gmail.com>
2024-04-21 10:44:22 -0700

    update util

--- util/vectorize_tweets.py
+++ util/vectorize_tweets.py
@@ -6,16 +6,18 @@ from sentence_transformers import SentenceTransformer
 # Initialize the model globally if it doesn't need to be reloaded each time
 model = SentenceTransformer("mixedbread-ai/mxbai-embed-large-v1")
 
+
 def vectorize_tweets(tweets):
     """
     Vectorizes a list of tweets using a Sentence Transformer model.
-    
+
     Args:
     tweets (list of str): A list of tweets to be vectorized.
-    
+
     Returns:
     tuple: A tuple containing the vector representations of the tweets and a matrix of cosine similarities.
     """
+    print(tweets)
     if not tweets:
         return []  # Return empty lists if the input list is empty
 
@@ -23,6 +25,7 @@ def vectorize_tweets(tweets):
     embeddings = model.encode(tweets)
 
     return embeddings
+
 
 # Example usage of the function
 # tweets = ["Tweet about food.", "Tweet about sports.", "Another tweet about politics."]
--- util/vectorized_centroids_to_topics.py
+++ util/vectorized_centroids_to_topics.py
@@ -1,4 +1,4 @@
-# This program provides an end-to-end solution for analyzing tweet vectors to determine the main topics being discussed. 
+# This program provides an end-to-end solution for analyzing tweet vectors to determine the main topics being discussed.
 # The process includes several key components:
 #
 # 1. KMeans Clustering: The function begins by applying the KMeans clustering algorithm to group tweet vectors into clusters.
@@ -8,33 +8,44 @@
 #    These vectors are considered representative of their respective clusters.
 #
 # 3. Summarizing Texts: Using the BART large model from the Hugging Face transformers library, the function then summarizes the tweets
-#    corresponding to these closest vectors. Summarization aims to condense the tweets into their essential messages, making it easier 
+#    corresponding to these closest vectors. Summarization aims to condense the tweets into their essential messages, making it easier
 #    to extract meaningful topics.
 #
 # 4. Extracting Topics: Subsequently, the T5 model is employed to further analyze the summaries and extract keywords or phrases.
 #    These serve as the topics for each cluster, representing the primary focus or subject matter of the grouped tweets.
 #
-# 5. Cleaning and Deduplication: Before finalizing the output, the topics undergo a cleaning process where commas are removed, 
+# 5. Cleaning and Deduplication: Before finalizing the output, the topics undergo a cleaning process where commas are removed,
 #    texts are converted to lowercase, and duplicate words are eliminated. This step ensures the clarity and uniqueness of each topic.
-from sklearn.cluster import KMeans
-import numpy as np
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, T5Tokenizer, T5ForConditionalGeneration
-from nltk.corpus import wordnet
 import nltk
+import numpy as np
+import pandas as pd
+from nltk.corpus import wordnet
+from sklearn.cluster import KMeans
+from transformers import (
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
+    T5ForConditionalGeneration,
+    T5Tokenizer,
+)
+from vectorize_tweets import vectorize_tweets
+
 # Download WordNet data
-nltk.download('words')
+nltk.download("words")
+
 
 # Function to remove @mentions
 def remove_mentions(tweet):
     # Regex to find mentions and handle spaces and punctuation
-    cleaned_tweet = re.sub(r'\s*@\w+\s*', ' ', tweet)
+    cleaned_tweet = re.sub(r"\s*@\w+\s*", " ", tweet)
     # Remove additional spaces created by replacements
-    cleaned_tweet = re.sub(r'\s+', ' ', cleaned_tweet).strip()
+    cleaned_tweet = re.sub(r"\s+", " ", cleaned_tweet).strip()
     return cleaned_tweet
-    
+
+
 def is_word_in_wordnet(word):
-    """ Check if a word is in WordNet """
+    """Check if a word is in WordNet"""
     return bool(wordnet.synsets(word))
+
 
 def filter_using_wordnet(tweets):
     filtered_tweets = []
@@ -42,8 +53,9 @@ def filter_using_wordnet(tweets):
         words_in_tweet = tweet.split()
         valid_words = [word for word in words_in_tweet if is_word_in_wordnet(word)]
         if valid_words:
-            filtered_tweets.append(' '.join(valid_words))
+            filtered_tweets.append(" ".join(valid_words))
     return filtered_tweets
+
 
 def find_closest_points(data, centroids):
     closest_indices = []
@@ -52,6 +64,7 @@ def find_closest_points(data, centroids):
         closest_index = np.argmin(distances)
         closest_indices.append(closest_index)
     return closest_indices
+
 
 def summarize_texts(texts):
     tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
@@ -64,6 +77,7 @@ def summarize_texts(texts):
         summaries.append(predicted)
     return summaries
 
+
 def extract_topics(summaries):
     tokenizer = T5Tokenizer.from_pretrained("Voicelab/vlt5-base-keywords")
     model = T5ForConditionalGeneration.from_pretrained("Voicelab/vlt5-base-keywords")
@@ -71,17 +85,21 @@ def extract_topics(summaries):
     task_prefix = "Keywords: "
     for summary in summaries:
         input_sequences = task_prefix + summary
-        input_ids = tokenizer(input_sequences, return_tensors="pt", truncation=True).input_ids
+        input_ids = tokenizer(
+            input_sequences, return_tensors="pt", truncation=True
+        ).input_ids
         output = model.generate(input_ids, no_repeat_ngram_size=3, num_beams=4)
         predicted = tokenizer.decode(output[0], skip_special_tokens=True)
         topics.append(predicted)
     return topics
 
+
 def clean_and_deduplicate(entry):
-    entry = entry.replace(',', '').lower()
+    entry = entry.replace(",", "").lower()
     words = entry.split()
     unique_words = list(set(words))
-    return ' '.join(unique_words)
+    return " ".join(unique_words)
+
 
 def analyze_tweet_vectors(tweet_vectors, tweets_list):
     kmeans = KMeans(n_clusters=20, random_state=42)
@@ -100,8 +118,13 @@ def analyze_tweet_vectors(tweet_vectors, tweets_list):
     filtered_tweets_wordnet = filter_using_wordnet(cleaned_topics)
     return filtered_tweets_wordnet
 
+
 # Example usage:
 # Assuming 'tweet_vectors' and 'tweets_list_new' are provided
-# topics = analyze_tweet_vectors(tweet_vectors, tweets_list_new)
-# print(topics)
 
+file_path_new = "./util/elonmusk.csv"
+data = pd.read_csv(file_path_new)
+tweets = data["Tweets"].tolist()
+tweet_vectors = vectorize_tweets(tweets)
+topics = analyze_tweet_vectors(tweet_vectors, tweets)
+print(topics)
